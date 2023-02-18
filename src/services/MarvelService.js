58223f492ddaf358f19a94ab39f1710a00b9462e
Natalie <natasha.pogorenko@gmail.com>
2023-02-18 23:10:06 +0200

    added comics page

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -8,6 +8,7 @@ const useMarvelService = () => {
     const _apiKey = 'apikey=86ab3206d3683fad9c48eea22faab2ec';
     const _baseOffset = 210;
 
+
     const getAllCharacters = async (offset = _baseOffset) => {
         const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_transformCharacter)
@@ -16,6 +17,16 @@ const useMarvelService = () => {
     const getCharacter = async (id) => {
         const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
         return _transformCharacter(res.data.results[0]);
+    }
+
+    const getAllComics = async (offset = _baseOffset) => {
+        const res = await request(`${_apiBase}comics?orderBy=focDate&limit=8&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics)
+    }
+
+    const getComic = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
     }
 
     const checkDescr = (descr) => {
@@ -38,7 +49,19 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, clearError, getAllCharacters, getCharacter}
+    const _transformComics = (comics) => {
+        return{
+            title: comics.title,
+            price: comics.prices[0].price + '$',
+            thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+            id: comics.id,
+            description: comics.description || 'no descr',
+            pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'no info',
+            language: comics.textObjects.language || 'en-us'
+        }
+    }
+
+    return {loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic}
 }
 
 export default useMarvelService;
